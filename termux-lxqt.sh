c88017bd879582f456e96c8c404f30e753f42eab
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-14 05:25:20 +0700

    Update termux-lxqt.sh

--- termux-lxqt.sh
+++ termux-lxqt.sh
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg install x11-repo
+pkg update
 pkg install -y tigervnc lxqt qterminal otter-browser
 echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
 echo 'lxqt-session &' >> ~/.vnc/xstartup
