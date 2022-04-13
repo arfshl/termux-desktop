912b92fc0d0839ab249b7c5c1ac587278520ad19
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-14 05:25:01 +0700

    Update and rename termux-mate.sh to mate.sh

--- mate.sh
+++ mate.sh
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg install x11-repo
+pkg update
 pkg install -y tigervnc mate-* marco mate-terminal netsurf
 echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
 echo 'mate-session &' >> ~/.vnc/xstartup
