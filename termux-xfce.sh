374ac5d7617d9c841a243f30354c4b22d5e835c4
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-14 04:49:30 +0700

    Create termux-xfce.sh

---
+++ termux-xfce.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install x11-repo
+pkg install tigervnc xfce4 xfce4-terminal netsurf
+echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
+echo 'xfce4-session &' >> ~/.vnc/xstartup
+echo 'export DISPLAY=":1"' >> ~/.bashrc
+vncserver -localhost
+
