16ab5dbfb62fe07efdf1f0b5db5da3bcef3d8e96
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-14 04:52:32 +0700

    Create termux-lxqt.sh

---
+++ termux-lxqt.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install x11-repo
+pkg install tigervnc lxqt qterminal otter-browser
+echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
+echo 'lxqt-session &' >> ~/.vnc/xstartup
+echo 'export DISPLAY=":1"' >> ~/.bashrc
+vncserver -localhost
