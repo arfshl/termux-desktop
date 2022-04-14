7809b749fa2104f8ebd88f0f399121ef6a990332
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-14 11:46:35 +0700

    Update openbox.sh

--- openbox.sh
+++ openbox.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
-pkg install x11-repo
-pkg update
+pkg install -y x11-repo
+pkg -y update
 pkg install -y tigervnc openbox pypanel xorg-xsetroot
-echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
 echo 'openbox-session &' >> ~/.vnc/xstartup
 echo 'xsetroot -solid gray' >> ~/.config/openbox/autostart
 echo 'pypanel &' >> ~/.config/openbox/autostart
+export DISPLAY=":1"
 echo 'export DISPLAY=":1"' >> ~/.bashrc
 vncserver -localhost
