29f2622aa970c3c5f55db12441749fece7254a52
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-25 17:42:29 +0700

    Update fluxbox.sh

--- fluxbox.sh
+++ fluxbox.sh
@@ -1,10 +1,13 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/data/data/com.termux/files/usr/bin/sh
 cd
-pkg install -y x11-repo
-pkg update -y
+echo "deb https://packages.termux.org/apt/termux-main stable main" >> $PREFIX/etc/apt/sources.list
+echo "deb https://packages.termux.org/apt/termux-x11/ x11 main" >> $PREFIX/etc/apt/sources.list
+pkg update 
 pkg install -y tigervnc fluxbox
 mkdir .vnc
+echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
 echo 'fluxbox-generate_menu' >> ~/.vnc/xstartup
 echo 'fluxbox &' >> ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
 export DISPLAY=":1"
 vncserver -localhost
