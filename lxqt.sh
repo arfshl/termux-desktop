6554a2cae56eb69af7a583c90cac827c054f40b2
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-25 17:37:47 +0700

    Update lxqt.sh

--- lxqt.sh
+++ lxqt.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd
-pkg install -y x11-repo
 pkg update
 pkg install -y tigervnc lxqt qterminal otter-browser
 mkdir .vnc
+echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
 echo 'lxqt-session &' >> ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
 export DISPLAY=":1"
-echo 'export DISPLAY=":1"' >> ~/.bashrc
 vncserver -localhost
