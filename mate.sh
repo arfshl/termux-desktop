081d5f636c0bc564ce6947e70f9ad3b2af91709b
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-25 17:23:55 +0700

    Update mate.sh

--- mate.sh
+++ mate.sh
@@ -1,9 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd
-pkg install -y x11-repo
-pkg update -y
+echo "deb https://packages.termux.org/apt/termux-main stable main" >> $PREFIX/etc/apt/sources.list
+echo "deb https://packages.termux.org/apt/termux-x11/ x11 main" >> $PREFIX/etc/apt/sources.list
+pkg update 
 pkg install -y tigervnc mate-* marco mate-terminal netsurf
 mkdir .vnc
 echo 'mate-session &' >> ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
 export DISPLAY=":1"
 vncserver -localhost
