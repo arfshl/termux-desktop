c72e8b7a7473bde6658ab88f46cc55ce48a10d92
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-25 17:37:16 +0700

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -5,6 +5,7 @@ echo "deb https://packages.termux.org/apt/termux-x11/ x11 main" >> $PREFIX/etc/a
 pkg update 
 pkg install -y tigervnc xfce4 xfce4-terminal netsurf
 mkdir .vnc
+echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
 echo 'xfce4-session &' >> ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
 export DISPLAY=":1"
