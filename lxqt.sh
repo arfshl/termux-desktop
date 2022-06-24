845a4cb1c30542a0a4cec0d3ea7c671581769201
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-24 17:23:11 +0700

    Update lxqt.sh

--- lxqt.sh
+++ lxqt.sh
@@ -1,7 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
+echo "Termux GUI installer (LXQt)"
 cd
+pkg install -y x11-repo
 pkg update
-pkg install -y tigervnc lxqt qterminal otter-browser
+pkg install -y tigervnc lxqt otter-browser qterminal openbox
 mkdir .vnc
 echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
 echo 'lxqt-session &' >> ~/.vnc/xstartup
