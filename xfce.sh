af9513238a23c91f59dee159f3bd789bc6932add
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-24 16:51:24 +0700

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,6 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo "Termux GUI installer (XFCE)
-echo "
+echo "cd
+echo "deb https://packages.termux.org/apt/termux-main stable main" >> $PREFIX/etc/apt/sources.list
+echo "deb https://packages.termux.org/apt/termux-x11/ x11 main" >> $PREFIX/etc/apt/sources.list
+pkg update -y
+pkg install -y tigervnc xfce4 xfce4-terminal netsurf
+mkdir .vnc
+echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
+echo 'xfce4-session &' >> ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
+export DISPLAY=":1"
+vncserver -localhost"
 cd
 echo "deb https://packages.termux.org/apt/termux-main stable main" >> $PREFIX/etc/apt/sources.list
 echo "deb https://packages.termux.org/apt/termux-x11/ x11 main" >> $PREFIX/etc/apt/sources.list
