caca15393b228ff03f82bdaa5d9fd4be8aff0406
arfshl <98870352+arfshl@users.noreply.github.com>
2022-09-19 20:32:24 +0700

    Update mate.sh

--- mate.sh
+++ mate.sh
@@ -7,7 +7,18 @@ mkdir .vnc
 echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
 echo 'mate-session &' >> ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
+echo '#!/data/data/com.termux/files/usr/bin/sh' >> /data/data/com.termux/files/usr/bin/vncstart
+echo 'vncserver -localhost' >> /data/data/com.termux/files/usr/bin/vncstart
+chmod +x /data/data/com.termux/files/usr/bin/vncstart
+echo '#!/data/data/com.termux/files/usr/bin/sh' >> /data/data/com.termux/files/usr/bin/vncstop
+echo 'vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/vncstop
+chmod +x /data/data/com.termux/files/usr/bin/vncstop
+echo '#!/data/data/com.termux/files/usr/bin/sh' >> /data/data/com.termux/files/usr/bin/restart
+echo 'vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/restart
+echo 'vncserver -localhost' >> /data/data/com.termux/files/usr/bin/restart
+chmod +x /data/data/com.termux/files/usr/bin/restart
+echo 'To start VNC use vncstart command'
+echo 'To stop VNC use vncstop command'
+echo 'To restart VNC use restart command'
 export DISPLAY=":1"
-echo 'To start VNC use vncserver -localhost command'
-echo 'To stop VNC use vncserver -kill :1 command'
 vncserver -localhost
