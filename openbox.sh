56a0623c9e2c1c94ea9b9d85603454fab38a3911
arfshl <98870352+arfshl@users.noreply.github.com>
2022-09-20 17:27:43 +0700

    Update openbox.sh

--- openbox.sh
+++ openbox.sh
@@ -8,17 +8,17 @@ echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
 echo 'openbox-session &' >> ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
 echo '#!/data/data/com.termux/files/usr/bin/sh' >> /data/data/com.termux/files/usr/bin/vncstart
-echo 'vncserver -localhost' >> /data/data/com.termux/files/usr/bin/vncstart
-chmod +x /data/data/com.termux/files/usr/bin/vncstart
+echo 'vncserver -localhost' >> /data/data/com.termux/files/usr/bin/start
+chmod +x /data/data/com.termux/files/usr/bin/start
 echo '#!/data/data/com.termux/files/usr/bin/sh' >> /data/data/com.termux/files/usr/bin/vncstop
-echo 'vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/vncstop
-chmod +x /data/data/com.termux/files/usr/bin/vncstop
+echo 'vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/stop
+chmod +x /data/data/com.termux/files/usr/bin/stop
 echo '#!/data/data/com.termux/files/usr/bin/sh' >> /data/data/com.termux/files/usr/bin/restart
 echo 'vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/restart
 echo 'vncserver -localhost' >> /data/data/com.termux/files/usr/bin/restart
 chmod +x /data/data/com.termux/files/usr/bin/restart
-echo 'To start VNC use vncstart command'
-echo 'To stop VNC use vncstop command'
+echo 'To start VNC use start command'
+echo 'To stop VNC use stop command'
 echo 'To restart VNC use restart command'
 export DISPLAY=":1"
 vncserver -localhost
