7aba9c896346a09688a9b856204a76029660d2ba
arfshl <98870352+arfshl@users.noreply.github.com>
2022-11-02 18:32:51 +0700

    Update openbox.sh

--- openbox.sh
+++ openbox.sh
@@ -4,19 +4,24 @@ cd
 pkg install -y x11-repo
 pkg install -y tigervnc openbox pypanel xorg-xsetroot
 mkdir .vnc
-echo '#!/data/data/com.termux/files/usr/bin/sh' >> ~/.vnc/xstartup
-echo 'openbox-session &' >> ~/.vnc/xstartup
+
+echo '#!/data/data/com.termux/files/usr/bin/sh
+openbox-session &' >> ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
-echo '#!/data/data/com.termux/files/usr/bin/sh' >> /data/data/com.termux/files/usr/bin/vncstart
-echo 'vncserver -localhost' >> /data/data/com.termux/files/usr/bin/start
+
+echo '#!/data/data/com.termux/files/usr/bin/sh
+vncserver -localhost' >> /data/data/com.termux/files/usr/bin/start
 chmod +x /data/data/com.termux/files/usr/bin/start
-echo '#!/data/data/com.termux/files/usr/bin/sh' >> /data/data/com.termux/files/usr/bin/vncstop
-echo 'vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/stop
+
+echo '#!/data/data/com.termux/files/usr/bin/sh
+vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/stop
 chmod +x /data/data/com.termux/files/usr/bin/stop
-echo '#!/data/data/com.termux/files/usr/bin/sh' >> /data/data/com.termux/files/usr/bin/restart
-echo 'vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/restart
-echo 'vncserver -localhost' >> /data/data/com.termux/files/usr/bin/restart
+
+echo '#!/data/data/com.termux/files/usr/bin/sh
+vncserver -kill :1
+vncserver -localhost' >> /data/data/com.termux/files/usr/bin/restart
 chmod +x /data/data/com.termux/files/usr/bin/restart
+
 echo 'To start VNC use start command'
 echo 'To stop VNC use stop command'
 echo 'To restart VNC use restart command'
