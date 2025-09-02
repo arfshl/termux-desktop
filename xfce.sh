f525cc0d47aace0f3abdcfd20c61ce248a68c9f9
Alif <arfshl@protonmail.com>
2025-09-02 19:45:02 +0700

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,29 +1,63 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "Installing XFCE..."
-apt install -y x11-repo
-apt install -y tigervnc xfce4 xfce4-terminal netsurf
-mkdir .vnc
-echo "Setting up VNC..."
+# Install x11 and tur repo
+termux-setup-storage
+apt update
+apt install x11-repo
+apt update
+
+# Update installed package but keep configuration
+apt upgrade -y -o Dpkg::Options::="--force-confold"
+
+# Install depedency
+apt install curl wget nano termux-x11 pulseaudio -y
+
+# Install XFCE
+apt install -y tigervnc xfce4 xfce4-* termux-x11 xfce4-terminal dbus-x11
+
+cat <<'EOF' > /data/data/com.termux/files/usr/bin/xfce-x11
+#!/bin/sh
+LD_PRELOAD=/system/lib64/libskcodec.so
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+export XDG_RUNTIME_DIR=${TMPDIR}
+kill -9 $(pgrep -f "termux.x11") 2>/dev/null
+kill -9 $(pgrep -f "x11") 2>/dev/nul
+termux-x11 :0 >/dev/null &
+sleep 3
+export PULSE_SERVER=127.0.0.1 
+export XDG_RUNTIME_DIR=${TMPDIR}
+DISPLAY=:0 dbus-launch --exit-with-session startxfce4
+EOF
+
+# Setting up VNC
+mkdir -p ~/.vnc
+
+# Create VNC password file (default 12345678)
+printf "12345678" | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
 
 echo '#!/data/data/com.termux/files/usr/bin/sh
-xfce4-session &' >> ~/.vnc/xstartup
+export PULSE_SERVER=127.0.0.1
+export DISPLAY=:1
+dbus-launch --exit-with-session startxfce4' >> ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
 
 echo '#!/data/data/com.termux/files/usr/bin/sh
-vncserver -localhost' >> /data/data/com.termux/files/usr/bin/start
-chmod +x /data/data/com.termux/files/usr/bin/start
+LD_PRELOAD=/system/lib64/libskcodec.so
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+vncserver :1
+echo "VNC server address: 127.0.0.1:5901 Password: 12345678"' >> /data/data/com.termux/files/usr/bin/startvnc
+chmod +x /data/data/com.termux/files/usr/bin/startvnc
 
 echo '#!/data/data/com.termux/files/usr/bin/sh
-vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/stop
-chmod +x /data/data/com.termux/files/usr/bin/stop
+vncserver -kill :1' >> /data/data/com.termux/files/usr/bin/stopvnc
+chmod +x /data/data/com.termux/files/usr/bin/stopvnc
 
 echo '#!/data/data/com.termux/files/usr/bin/sh
 vncserver -kill :1
-vncserver -localhost' >> /data/data/com.termux/files/usr/bin/restart
-chmod +x /data/data/com.termux/files/usr/bin/restart
+vncserver :1' >> /data/data/com.termux/files/usr/bin/restartvnc
+chmod +x /data/data/com.termux/files/usr/bin/restartvnc
 
-echo 'To start VNC use start command'
-echo 'To stop VNC use stop command'
-echo 'To restart VNC use restart command'
-export DISPLAY=":1"
-vncserver -localhost
+echo 'To start VNC use startvnc command'
+echo 'To stop VNC use stopvnc command'
+echo 'To restart VNC use restartvnc command'
+startvnc
