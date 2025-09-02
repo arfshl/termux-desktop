21ef5deea1fdb7469f3e56dde2ab21a5f45658dd
Alif <arfshl@protonmail.com>
2025-09-02 20:04:13 +0700

    Update lxqt.sh

--- lxqt.sh
+++ lxqt.sh
@@ -11,7 +11,7 @@ apt upgrade -y -o Dpkg::Options::="--force-confold"
 # Install depedency
 apt install curl wget nano -y
 
-# Install XFCE
+# Install LXQt
 apt install -y tigervnc termux-x11 pulseaudio dbus-x11 lxqt
 
 cat <<'EOF' > /data/data/com.termux/files/usr/bin/lxqt-x11
