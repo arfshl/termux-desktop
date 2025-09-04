f9715ea7a97487a10ab5f0da4d2e01826616a6df
Alif <arfshl@protonmail.com>
2025-09-04 08:00:56 +0700

    Update mate.sh

--- mate.sh
+++ mate.sh
@@ -12,7 +12,7 @@ apt upgrade -y -o Dpkg::Options::="--force-confold"
 apt install curl wget nano -y
 
 # Install MATE
-apt install -y tigervnc termux-x11 pulseaudio dbus-x11 mate-* marco mate-terminal caja 
+apt install -y tigervnc termux-x11 pulseaudio dbus mate-* marco mate-terminal caja 
 
 cat <<'EOF' > /data/data/com.termux/files/usr/bin/startx11
 #!/bin/sh
